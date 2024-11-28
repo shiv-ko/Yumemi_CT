71b992868f53b9964db9cbbc4ca31bf61a461771
shiv-ko <iscopre7@gmail.com>
2024-11-28 20:48:16 +0900

    style: PopulationTypeSelectorのスタイルを追加

--- app/components/PopulationTypeSelector.tsx
+++ app/components/PopulationTypeSelector.tsx
@@ -1,3 +1,5 @@
+import '../styles/PopulationTypeSelector.css'; 
+
 type Props = {
   selectedType: string; // 選択された人口タイプ
   setSelectedType: (type: string) => void; // 人口タイプを更新する関数
@@ -12,26 +14,28 @@ export default function PopulationTypeSelector({
   // チェックボックスの変更ハンドラー
   const handleCheckboxChange = (type: string) => {
     if (selectedType === type) {
-      // クリックしたチェックボックスが既に選択されている場合、選択を解除
       setSelectedType('');
     } else {
-      // 他のチェックボックスが選択されている場合、それを解除し、新たに選択
       setSelectedType(type);
     }
   };
 
   return (
-    <div>
-      <h2>人口タイプ</h2>
+    <div className="population-type-selector-container">
       {populationTypes.map((type) => (
-        <label key={type} style={{ marginRight: '10px' }}>
+        <label
+          key={type}
+          className="population-type-selector-label"
+          style={{ marginRight: '10px' }}
+        >
           <input
             type="checkbox"
             value={type}
             checked={selectedType === type}
             onChange={() => handleCheckboxChange(type)}
+            className="population-type-selector-checkbox"
           />
-          {type}
+          <span className="population-type-selector-text">{type}</span>
         </label>
       ))}
     </div>
